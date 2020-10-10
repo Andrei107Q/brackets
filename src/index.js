ce412d302d268f65904f6af6f6f6e27efca00cdf
Andrei <denisenko.andrei.da@gmail.com>
2020-10-10 22:46:16 +0300

    feat: created function for check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if(str.length % 2 !== 0) return false;
+  let coupleBrackets = [];
+
+  bracketsConfig.flat().forEach((element, index) => {
+    if((index + 1) % 2 !== 0) {
+      coupleBrackets.push(bracketsConfig.flat()[index] + bracketsConfig.flat()[index + 1]);
+    };
+  });
+
+
+  for(let i = 0; i < str.length; i++) {
+    coupleBrackets.forEach(element => { 
+      if(str.includes(element)) {
+        str = str.replace(element, '');
+        i--;
+      };
+    });
+  };
+  return str === '';
+};
